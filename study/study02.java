329e16faf4af8cf9e5a4587950af61529241694c
Roma_Man <62382377+kjspo56@users.noreply.github.com>
2021-02-01 18:59:29 +0900

    Add files via upload
    백준 1000번 // 1001번

---
+++ study/study02.java
@@ -0,0 +1,19 @@
+//백준 1001번
+package study;
+
+import java.util.Scanner;
+
+public class study02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			Scanner sc = new Scanner(System.in);
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			sc.close();
+			
+			System.out.println(a-b);
+	}
+
+}
