a4f300508f14066b5acfabe6f44debffec4ea347
Roma_Man <62382377+kjspo56@users.noreply.github.com>
2021-02-04 15:44:00 +0900

    Add files via upload

---
+++ 210204/src/study/study01.java
@@ -0,0 +1,17 @@
+//백준 10998번
+package study;
+
+import java.util.Scanner;
+
+public class study01 {
+	public static void main (String[] args){
+		
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		sc.close();
+		
+		System.out.println(a*b);
+		
+	}
+}
