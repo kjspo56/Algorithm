a10a7ae8cf28de10b692f253be3b3c1d54322121
kjspo56 <kjspo56@gmail.com>
2021-12-01 18:27:34 +0900

    Bronze5 맛보기

---
+++ BJ/Bronze5/BJ11654.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class BJ11654 {
+    public static void main(String[] args) throws IOException {
+        int a = System.in.read();
+        System.out.println(a);
+    }
+}
