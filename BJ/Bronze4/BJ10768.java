6ad9b68185167cbb44439724fed1df2391cb6ab5
kjspo56 <kjspo56@gmail.com>
2021-12-29 17:55:55 +0900

    add 10768

---
+++ BJ/Bronze4/BJ10768.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ10768 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int Month = Integer.parseInt(br.readLine());    //월 입력
+        int Day = Integer.parseInt(br.readLine());    //달 입력
+
+        if (Month < 2){
+            System.out.println("Before");
+        } else if (Month == 2) {
+            if(Day < 18) {
+                System.out.println("Before");
+            } else if(Day > 18) {
+                System.out.println("After");
+            } else {
+                System.out.println("Special");
+            }
+        } else {
+            System.out.println("After");
+        }
+    }
+}
