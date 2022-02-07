b53ee6c417819f1bd72e0ae3c2f941af979b9841
kjspo56 <kjspo56@gmail.com>
2022-02-07 11:53:04 +0900

    add 1864

--- BJ/Bronze3/BJ1837.java
+++ BJ/Bronze3/BJ1837.java
@@ -9,17 +9,21 @@ public class BJ1837 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        BigInteger p = new BigInteger(st.nextToken());  //암호P
+        BigInteger P = new BigInteger(st.nextToken());  //암호P
 
         int K = Integer.parseInt(st.nextToken());   //암호 K
 
         boolean[] promise = new boolean[K + 1];
         promise[1] = true;
 
+
+        /*
+        소수를 구할때, 에라토스테네스의 체를 사용. 2부터 시작하여 소수의 배수를 모두 걸러냄.
+         */
         for(int i = 2; i < K; i++) {
             if (promise[i]) continue; //소수가 아닌(true)는 수는 넘어가기기
             BigInteger now = new BigInteger(Integer.toString(i));
-            if (p.mod(now).compareTo(BigInteger.ZERO) == 0) {    // p를 now로 나눈 나머지가 0이면,
+            if (P.mod(now).compareTo(BigInteger.ZERO) == 0) {    // p를 now로 나눈 나머지가 0이면,
                 System.out.println("BAD " + now);
                 return;
             }
