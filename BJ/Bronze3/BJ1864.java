11c8416c2a57c9703946f175dc6e98d4ae50f004
kjspo56 <kjspo56@gmail.com>
2022-02-08 11:37:17 +0900

    add 1964

--- BJ/Bronze3/BJ1864.java
+++ BJ/Bronze3/BJ1864.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.StringTokenizer;
 
 public class BJ1864 {
     public static void main(String[] args) throws IOException {
