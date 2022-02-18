8898f74490fb42781d07f0d0486ed4106aac9acf
kjspo56 <kjspo56@gmail.com>
2022-02-18 13:46:17 +0900

    add 2440

--- BJ/Bronze3/BJ2440.java
+++ BJ/Bronze3/BJ2440.java
@@ -2,21 +2,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class BJ2439 {
+public class BJ2440 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
 
-        for(int i=1; i<=N; i++){
-            for(int j=N; j>0;j--){
-                if(i<j){
-                    System.out.print(" ");
-                } else {
-                    System.out.print("*");
-                }
+        for(int i = 0; i<N; i++){
+            for(int j = i+1; j<=N; j++){
+                System.out.print("*");
             }
-            System.out.println("");
+            System.out.println();
         }
     }
 }
