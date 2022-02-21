8c43d38650f1b57bd81af90800740c987862a9f9
kjspo56 <kjspo56@gmail.com>
2022-02-21 16:46:29 +0900

    add 10996 2522

---
+++ BJ/Bronze3/BJ2522.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ2522 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        //위쪽
+        for(int i = 1; i <= N; i++){
+            for(int j = 1; j<=N-i; j++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        //아래쪽
+        for(int i = 1; i<=N-1; i++){
+            for(int j = 1; j<=i; j++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j<=N-i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
