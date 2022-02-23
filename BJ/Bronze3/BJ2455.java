160e4f3b4dbdfe062023542cae1dc9f30318e7d3
kjspo56 <kjspo56@gmail.com>
2022-02-23 14:47:32 +0900

    add 2455 Buffer로 modify

--- BJ/Bronze3/BJ2455.java
+++ BJ/Bronze3/BJ2455.java
@@ -1,48 +1,22 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class BJ2455 {
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        //1번
-        int a1 = sc.nextInt();
-        int b1 = sc.nextInt();
+        int max = 0;
+        int now = 0;
+        for(int i = 0; i<4; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int out = Integer.valueOf(st.nextToken());
+            int in = Integer.valueOf(st.nextToken());
 
-        //2번
-        int a2 = sc.nextInt();
-        int b2 = sc.nextInt();
-
-        //3번
-        int a3 = sc.nextInt();
-        int b3 = sc.nextInt();
-
-        //4번
-        int a4 = sc.nextInt();
-        int b4 = sc.nextInt();
-
-        //1번째 총 인원수
-        int A1 = a1 + b1;
-
-        //2번째 총 인원수
-        int A2 = A1 - a2 + b2;
-
-        //3번째 총 인원수
-        int A3 = A2 - a3 + b3;
-
-        //4번째 총 인원수
-        int A4 = A3 - a4 + b4;
-
-        int max = A1;
-        if(A2> max) max = A2;
-        if(A3> max) max = A3;
-        if(A4> max) max = A4;
-
+            now = now - out + in;
+            max = Math.max(max, now);
+        }
         System.out.println(max);
-
     }
 }
