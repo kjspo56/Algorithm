45e049a4a2479151f602ea04126a57bd8cf48b02
kjspo56 <kjspo56@gmail.com>
2022-03-15 10:42:34 +0900

    add 2741 2742

--- BJ/Bronze3/BJ2741.java
+++ BJ/Bronze3/BJ2741.java
@@ -1,11 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class BJ2741 {
-    public static void  main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-
-        int N = scanner.nextInt();
-        scanner.close();
+    public static void  main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
         for (int i=1; i<=N;i++){
             System.out.println(i);
