d9987e0f99c8eaf56a7b6fdea07859570052cd53
kjspo56 <kjspo56@gmail.com>
2022-03-29 11:34:13 +0900

    add 2953

--- Solution/BackJoon/Bronze3/BJ2953.java
+++ Solution/BackJoon/Bronze3/BJ2953.java
@@ -13,7 +13,7 @@ public class BJ2953 {
         int max = 0;    //최대값을 저장할 변수
         int n = 0;  //몇번째 사람인지 저장할 변수
 
-        for(int i = 0; i<5; i++){   //다섯명의 사람 측정
+        for(int i = 1; i<=5; i++){   //다섯명의 사람 측정
             StringTokenizer st = new StringTokenizer(br.readLine());
             int sum = 0; //점수들의 합
             for(int j =0; j < 4; j++) {//점수들의 합을 sum에 계속 저장한다.
@@ -21,7 +21,7 @@ public class BJ2953 {
             }
             if(max < sum) { //max가 sum보다 작을 경우에 sum값을 n에 i+1형태로 저장한다.
                 max = sum;
-                n = i + 1;
+                n = i ;
             }
         }
         System.out.println(n + " " + max);
