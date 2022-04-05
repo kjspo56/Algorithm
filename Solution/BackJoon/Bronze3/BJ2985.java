7a62eca14023a7f51fb26c5f4537b07208a123d5
kjspo56 <kjspo56@gmail.com>
2022-04-05 10:17:14 +0900

    add 2985

---
+++ Solution/BackJoon/Bronze3/BJ2985.java
@@ -0,0 +1,37 @@
+package Bronze3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ2985 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        //연산에 따라 출력한다.
+        if(A + B == C) {
+            System.out.println(A + "+" + B + "=" + C);
+        } else if (A - B == C) {
+            System.out.println(A + "-" + B + "=" + C);
+        } else if (A * B == C) {
+            System.out.println(A + "*" + B + "=" + C);
+        } else if (A / B == C) {
+            System.out.println(A + "/" + B + "=" + C);
+        } else if (A == B + C) {
+            System.out.println(A + "=" + B + "+" + C);
+        } else if (A == B - C) {
+            System.out.println(A + "=" + B + "-" + C);
+        } else if (A == B * C) {
+            System.out.println(A + "=" + B + "*" + C);
+        } else if (A == B / C) {
+            System.out.println(A + "=" + B + "/" + C);
+        }
+    }
+}
